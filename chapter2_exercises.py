943371007942032e92fa7d45be53b2bdee183ec1
danielAbrams <daniel@kitchenatpenn.com>
2014-02-01 00:41:42 -0500

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,51 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:Daniel Abrams
+
+#Exercise 2.1 
+#Python is seems to be using base 8 for all entries that begin with 0.
+#This is backed up by the fact that the entry of any number greater than 7 after a 0 returns an error.
+#This is also backed up by the fact that 010 is 8, 0100 is 64 and so on.
+
+#Exercise 2.2
+print 5
+x = 5
+print x+1
+#prints 5 then prints 6
+#we cannot print x = 5 because it is not an int. We can convert it to a string, but then it does not adjust the value of x.
+
+#Exercise 2.3
+# 1. (int) 8
+# 2. (float) 8.5
+# 3. (float) 4.0
+# 4. (int) 11
+# 5. (string) .....
+
+#Exercise 2.4
+# 1.
+(4/3)*(3.1415926539)*(5^3)
+# is equal to 18.8495559234
+
+# 2.
+bookCost = 24.95*(0.6)*60 
+# Total Cost of books is 898.1999999999999
+shippingCost = 3 + (59*.75)
+#shipping costs equals 47.25
+totalCost = bookCost + shippingCost
+#Total Wholesale cost is 945.4499999999999
+
+# 3.
+easyPaceTime = (8*60)+15
+# Easy Pace in seconds = 495
+fastPaceTime = (7*60)+12
+# Fast Pace in seconds = 432
+totalTime = (easyPaceTime*2) + (3*fastPaceTime)
+#total time is 2286
+totalMinutes = totalTime/60
+totalSeconds = totalTime -(totalMinutes*60)
+#total Minutes is 38
+#total Seconds is 6
+#return home at 7:50:06
+ 
+
+
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,69 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Daniel Abrams
+
+#Exercise 3.1
+
+#repeat_lyrics()
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+#Error: NameError: name 'repeat_lyrics' is not defined
+print ' '
+print 'Exercise 3.2' 
+print ' '
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+repeat_lyrics()
+
+#error message:
+#Traceback (most recent call last):
+#  File "chapter3_exercises.py", line 7, in <module>
+#    repeat_lyrics()
+#NameError: name 'repeat_lyrics' is not defined
+
+#it works as before, because no functions are called until repeat_lyrics()
+
+print ' '
+print 'Exercise 3.3'
+print ' '
+def right_justify(s):
+	length = len(s)
+	numSpaces = (70 - length)
+	print (' '*numSpaces)+s
+
+right_justify('Tom')
+right_justify('Steve')
+
+print ' '
+print 'Exercise 3.4'
+print ' '
+
+def do_twice(f, i):
+	f(i)
+	f(i)
+
+def print_twice(string):
+	print string
+
+do_twice(print_twice, 'spam')
+
+def do_four(f, i):
+	do_twice(f,i)
+	do_twice(f,i)
+
+
+do_four(print_twice, 'four')
